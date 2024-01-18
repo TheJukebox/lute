ac39ecba66164dd7bf8217b602c780588c40b73f
abaduser <commits@abaduser.pw>
2024-01-18 15:39:36 -0800

    yeah its a start

---
+++ src/webapp/src/routes/mediaplayer.svelte
@@ -0,0 +1,13 @@
+<script>
+    let endpoint = 
+</script>
+
+<!-- Markup -->
+<audio id="Mediaplayer" controls>
+    <source src="source.mpd">
+    Your browser does not support the audio player.
+</audio>
+
+<style>
+
+</style>

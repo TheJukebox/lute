34c7c3dc2be09393054cd5ec0d6ebd53d979afa1
abaduser <Isaac.K.Masood@gmail.com>
2024-01-19 15:04:21 -0800

    dashboard

---
+++ src/webapp/src/routes/dashboard.svelte
@@ -0,0 +1,16 @@
+<script>
+    // script
+    let endpoint = "http://localhost:8000"
+    let desired_mpd;
+    
+</script>
+
+<div>
+    <Mediaplayer/>
+</div>
+
+<style>
+    /* style */
+</style>
+
+<!-- Markup -->

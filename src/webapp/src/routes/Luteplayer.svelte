9c3e1153a60d666c6360801a32435cba2b7d52c0
abaduser <Isaac.K.Masood@gmail.com>
2024-01-19 15:04:10 -0800

    created luteplayer concept, nest in dashboard

---
+++ src/webapp/src/routes/Luteplayer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let current_audio: string;
+</script>
+
+<!-- Markup -->
+<audio id="luteplayer" controls>
+    <source src={current_audio}>
+    Your browser does not support the audio player.
+</audio>
+
+<style>
+
+</style>

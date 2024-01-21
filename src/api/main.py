bc7c89df1b876f3a1ad5c5e8744d731e3be50fb2
the jukebox <grimshawjoshua2@gmail.com>
2024-01-21 22:52:56 +0000

    Fix bug for "file is dir" and "file already exists" when transcoding

--- src/api/main.py
+++ src/api/main.py
@@ -22,6 +22,12 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="src/api/static"), name="static")
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
 @app.get("/mpd/{uid}")

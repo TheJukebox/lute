d7a51f824ee5386d402bf574d4d6658f240d2318
jukebox <5196313+TheJukebox@users.noreply.github.com>
2024-11-28 08:22:24 +1100

    Implement tutorial gRPC

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "lute/gen"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
 
 func main() {
-	fmt.Println("hello world")
+	log.Println("Starting Lute server...")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module lute
 go 1.23.3
 
 require (
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.35.2
+)
+
+require (
 	github.com/xfrr/goffmpeg v1.0.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
 )

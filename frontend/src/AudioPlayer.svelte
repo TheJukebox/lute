8d801c003e3df4d2b9a91c3992a81e322010cd49
jukebox <5196313+TheJukebox@users.noreply.github.com>
2024-11-29 09:01:51 +1100

    Create layout proto for player

--- frontend/src/AudioPlayer.svelte
+++ frontend/src/AudioPlayer.svelte
@@ -1,145 +1,85 @@
-<script>
+<script lang='ts'>
 	let { src, title, artist } = $props();
 
-	let time = $state(0);
-	let duration = $state(0);
-	let paused = $state(true);
+	let time: number = $state(0);
+	let duration: number = $state(0);
+	let paused: boolean = $state(true);
 
-	function format(time) {
+	function format(time: number): string {
 		if (isNaN(time)) return '...';
 
-		const minutes = Math.floor(time / 60);
-		const seconds = Math.floor(time % 60);
+		const minutes: number = Math.floor(time / 60);
+		const seconds: number = Math.floor(time % 60);
 
 		return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 	}
 </script>
 
-<div class="player" class:paused>
-	<audio
-		{src}
-		bind:currentTime={time}
-		bind:duration
-		bind:paused
-		onended={() => {
-			time = 0;
-		}}
-	></audio>
+<div class='player' class:paused>
+	<audio>
 
-	<button
-		class="play"
-		aria-label={paused ? 'play' : 'pause'}
-		onclick={() => paused = !paused}
-	></button>
-
-	<div class="info">
-		<div class="description">
-			<strong>{title}</strong> /
-			<span>{artist}</span>
-		</div>
-
-		<div class="time">
-			<span>{format(time)}</span>
-			<div
-				class="slider"
-				onpointerdown={e => {
-					const div = e.currentTarget;
-
-					function seek(e) {
-						const { left, width } = div.getBoundingClientRect();
-
-						let p = (e.clientX - left) / width;
-						if (p < 0) p = 0;
-						if (p > 1) p = 1;
-
-						time = p * duration;
-					}
-
-					seek(e);
-
-					window.addEventListener('pointermove', seek);
-
-					window.addEventListener('pointerup', () => {
-						window.removeEventListener('pointermove', seek);
-					}, {
-						once: true
-					});
-				}}
-			>
-				<div class="progress" style="--progress: {time / duration}%"></div>
-			</div>
-			<span>{duration ? format(duration) : '--:--'}</span>
-		</div>
+	</audio>
+	<div class='albumArt'>
+	</div>
+	<div class='details'>
+		<p class='title'>Weezer Does Weezer</p>
+		<p class='artist'>Weezer</p>
+		<p class='album'>Weezer: The Weezer Years</p>
+	</div>
+	<div class='seek'>
 	</div>
 </div>
 
 <style>
 	.player {
-		display: grid;
-		grid-template-columns: 2.5em 1fr;
-		align-items: center;
-		gap: 1em;
-		padding: 0.5em 1em 0.5em 0.5em;
-		border-radius: 2em;
-		background: var(--bg-1);
-		transition: filter 0.2s;
-		color: var(--fg-3);
-		user-select: none;
+		position: relative;
+		border: 2px dashed #000;
+		width: 600px;
+		height: 100px;
+		background-color: #f9f9f9;
 	}
 
-	.player:not(.paused) {
-		color: var(--fg-1);
-		filter: drop-shadow(0.5em 0.5em 1em rgba(0,0,0,0.1));
+	.albumArt {
+		position: absolute;
+		height: 90%;
+		width: 90px;
+		border: 0px solid #15ff00;
+		background-color: #ff0000;
 	}
-
-	button {
+	
+	.seek {
+		position: absolute;
+		bottom: 0;
+		height: 10%;
 		width: 100%;
-		aspect-ratio: 1;
-		background-repeat: no-repeat;
-		background-position: 50% 50%;
-		border-radius: 50%;
+		border: 0px solid #00bbff;
+		background-color: #8b8b8b;
 	}
 
-	[aria-label="pause"] {
-		background-image: url(./assets/pause.svg);
+	.details {
+		position: absolute;
+		left: 90px;
+		bottom: 10%;
+		top: 0;
+		justify-content: start;
+		background-color: #ffffff;
+	}
+	
+	.title {
+		font-size: medium;
+		margin: 2px 10px 0 2px;
+	}
+	
+	.artist {
+		font-size: medium;
+		margin: 0 10px 0 2px;
 	}
 
-	[aria-label="play"] {
-		background-image: url(./assets/play.svg);
-	}
-
-	.info {
-		overflow: hidden;
-	}
-
-	.description {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		line-height: 1.2;
-	}
-
-	.time {
-		display: flex;
-		align-items: center;
-		gap: 0.5em;
-	}
-
-	.time span {
-		font-size: 0.7em;
-	}
-
-	.slider {
-		flex: 1;
-		height: 0.5em;
-		background: var(--bg-2);
-		border-radius: 0.5em;
-		overflow: hidden;
-	}
-
-	.progress {
-		width: calc(100 * var(--progress));
-		height: 100%;
-		background: var(--bg-3);
+	.album {
+		position: absolute;
+		bottom: 0;
+		font-size: small;
+		margin: 0;
+		font-style: oblique;
 	}
 </style>

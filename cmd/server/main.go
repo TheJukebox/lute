3846d9b75eb58f7a163c4193b21b4c56372c51f1
jukebox <5196313+TheJukebox@users.noreply.github.com>
2024-11-30 11:03:39 +1100

    Refactor to upload.proto and write data

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,31 @@ import (
 	"context"
 	"log"
 	"net"
+	"os"
 
-	pb "lute/gen/converter"
+	pb "lute/gen/upload"
 
 	"google.golang.org/grpc"
 )
 
-type converter struct {
-	pb.UnimplementedConverterServiceServer
+type uploadService struct {
+	pb.UnimplementedUploadServer
 }
 
-func (s *converter) ConvertToHLS(_ context.Context, in *pb.FileUploadRequest) (*pb.FileUploadResponse, error) {
+func (s *uploadService) FileUpload(_ context.Context, in *pb.FileUploadRequest) (*pb.FileUploadResponse, error) {
 	log.Printf("Received File Upload Request: %v", in.GetFileName())
+	output, err := os.Create(in.GetFileName())
+	if err != nil {
+		log.Printf("Could not open file for write: %s\n", in.GetFileName())
+		return &pb.FileUploadResponse{Success: false, Message: "File failed to upload: could not open file for write"}, err
+	}
+	defer output.Close()
+
+	_, err = output.Write(in.GetFileData())
+	if err != nil {
+		log.Printf("Could not write file: %s\n", in.GetFileName())
+		return &pb.FileUploadResponse{Success: false, Message: "Failed to write file."}, err
+	}
 	return &pb.FileUploadResponse{Success: true, Message: "Successfully uploaded"}, nil
 }
 
@@ -26,7 +39,7 @@ func main() {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterConverterServiceServer(s, &converter{})
+	pb.RegisterUploadServer(s, &uploadService{})
 	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)

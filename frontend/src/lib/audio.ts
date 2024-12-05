942c1fd0829841eca629f1a3cdccc915435873ea
jukebox <5196313+TheJukebox@users.noreply.github.com>
2024-12-05 17:43:46 +1100

    Frame detection algorithm

--- frontend/src/lib/audio.ts
+++ frontend/src/lib/audio.ts
@@ -58,18 +58,58 @@ export function fetchStream(host: string, path: string, sessionId: string) {
         // @ts-ignore - for some reason it thinks it's going to be a string
         // after assignment...
         const data: Uint8Array = response.getData(); 
-        dataQueue = dataQueue.concat(data);
+        const frameAt = containsADTSFrame(data)
+        if (containsADTSFrame(data) !== -1) {
+            console.log("FRAME AT: ", frameAt);
+        }
+
     });
 
     audioStream.on('end', async () => {
-        console.log(dataQueue);
-        let blob = new Blob(dataQueue);
-        console.log(blob);
-        let buff = await blob.arrayBuffer();
-        console.log(buff);
-        let audio = await decodeAudio(buff);
-        console.log(audio);
-        audioQueue.push(audio);
-        playFromBuffer();
     })
+}
+
+function containsADTSFrame(data: Uint8Array) {
+    for (let i = 0; i < data.length - 7; i++) {
+        if (data[i] === 0xFF && (data[i + 1] & 0xF0) === 0xF0) {
+            // check if the ID = 0 (MPEG-4).
+            if ((data[i + 1] & 0x08) >> 3 !== 0) {
+                break
+            }
+            // layer
+            if ((data[i + 1] & 0x06) >> 1 !== 0) {
+                break
+            }
+            // protection_absent
+            if ((data[i + 1] & 0x01) !== 0x01) {
+                // i think most of the time it should be present, so...
+                break
+                // that said, make sure we revisit this
+            }
+            // sampling frequency
+            if ((data[i + 2] & 0xF0) >> 4 !== 0x05) {
+                break;
+            }
+            // private_bit
+            if ((data[i + 2] & 0x08) >> 3 !== 0) {
+                // sometimes, very rarely, this bit can be 1
+                break;
+                // revisit it
+            }
+            // channel configuration - i'd actually expect this to be 2,
+            // but the file i'm working with is weird.
+            if ((data[i + 2] & 0x07) !== 0) {
+                break;
+            }
+            // it's a copy!
+            if ((data[i + 3] & 0xC0) >> 6 === 1) {
+                break;
+            }
+            if ((data[i + 3] & 0x20) >> 5 !== 0) {
+                break;
+            }
+            return i;
+        }
+    }
+    return -1;
 }

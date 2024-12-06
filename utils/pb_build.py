3665d9fca5f4de6fc8737b83ffa0ab3d5ac39fd2
jukebox <5196313+TheJukebox@users.noreply.github.com>
2024-12-06 16:21:32 +1100

    Comments and style

--- utils/pb_build.py
+++ utils/pb_build.py
@@ -7,10 +7,15 @@ import click
 
 
 def build_proto(path: Path) -> Path | Exception:
+    """Executes protoc to compile a protobuffer."""
+
+    # The protoc command to run
     cmd = f"protoc -I {path.parent} --go_out=. --go-grpc_out=. {path}"
-    # protoc -I api/proto stream.proto --js_out=import_style=commonjs,binary:frontend/src/lib/gen --grpc-web_out=import_style=typescript,mode=grpcwebtext:frontend/src/lib/gen
     try:
+        # Run the command and withold stderr so we can use it in our own
+        # output.
         ps.run(cmd.split(" "), check=True, stderr=ps.PIPE)
+        # if it succeeds, return the compiled code
         return Path(f"gen/{path.stem}/{path.stem}.pb.go")
     except ps.CalledProcessError as e:
         click.echo(click.style(f"\nFailed to build {path}!", fg="red"))
@@ -18,7 +23,9 @@ def build_proto(path: Path) -> Path | Exception:
         return None
 
 
-def find_proto_dir(d: Path =".") -> Path:
+def find_proto_dir(d: Path = ".") -> Path:
+    """Searches the directory tree to find directories containing files with the `.proto` extension."""
+
     for d, dirs, files in os.walk(d):
         if any(file.endswith(".proto") for file in files):
             return d
@@ -26,15 +33,19 @@ def find_proto_dir(d: Path =".") -> Path:
 
 
 def build_all(path: Path) -> Path:
+    """Builds all protobuffers found in the directory tree."""
+
     proto_dir = find_proto_dir(path)
     if not proto_dir:
         raise click.exceptions.ClickException("Could not find any .proto files!")
 
+    # Create a list of .proto files in the directory.
+    # We make sure to traverse its tree as well.
     proto_files = []
     for d, dirs, files in os.walk(proto_dir):
-        found_files = [f"{d}/{file}" for file in files if file.endswith('.proto')]
+        found_files = [f"{d}/{file}" for file in files if file.endswith(".proto")]
         proto_files.extend(found_files)
-            
+
     click.echo(f"Found {len(proto_files)} .proto files: ")
     for proto in proto_files:
         click.echo(f"\t{proto}")
@@ -47,9 +58,15 @@ def build_all(path: Path) -> Path:
 
 
 @click.command()
-@click.option("--all", is_flag=True, help="Compiles all protobuffers found in the current directory tree.")
+@click.option(
+    "--all",
+    is_flag=True,
+    help="Compiles all protobuffers found in the current directory tree.",
+)
 @click.argument("path", required=True)
 def build(path: Path, all: bool):
+    """A click command for compiling protobuffers."""
+
     if all:
         build_all(Path(path))
         exit(0)
@@ -57,7 +74,7 @@ def build(path: Path, all: bool):
     built = build_proto(Path(path))
     if built and built.exists():
         click.echo(click.style(f"Successfully built '{built}' !", fg="green"))
-        
+
 
 if __name__ == "__main__":
     build()

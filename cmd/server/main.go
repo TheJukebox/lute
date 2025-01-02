0159f0e8124900bc9a231caf06928c1a5d88d9a2
jukebox <5196313+TheJukebox@users.noreply.github.com>
2025-01-03 10:41:35 +1100

    Merge main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,63 +14,9 @@ import (
 	"google.golang.org/grpc"
 )
 
-<<<<<<< HEAD
 func debugSetup() {
 	os.Mkdir("uploads/raw", 0700)
 	os.Mkdir("uploads/converted", 0700)
-=======
-type streamService struct {
-	streamPb.UnimplementedAudioStreamServer
-}
-
-type uploadService struct {
-	uploadPb.UnimplementedUploadServer
-}
-
-func (s *streamService) StreamAudio(request *streamPb.AudioStreamRequest, stream streamPb.AudioStream_StreamAudioServer) error {
-	file, err := os.Open(request.GetFileName())
-	if err != nil {
-		log.Printf("Failed to open file for streaming: %q\n", err)
-		return err
-	}
-	defer file.Close()
-
-	streamBuffer := make([]byte, 8192*5)
-	sequence := int32(0)
-
-	for {
-		chunkSize, err := file.Read(streamBuffer)
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			log.Printf("Failed to chunk stream: %q\n", err)
-		}
-		chunk := &streamPb.AudioStreamChunk{
-			Data:     streamBuffer[:chunkSize],
-			Sequence: sequence,
-		}
-		sequence++
-		stream.Send(chunk)
-	}
-}
-
-func (s *uploadService) FileUpload(_ context.Context, in *uploadPb.FileUploadRequest) (*uploadPb.FileUploadResponse, error) {
-	log.Printf("Received File Upload Request: %v", in.GetFileName())
-	output, err := os.Create(in.GetFileName())
-	if err != nil {
-		log.Printf("Could not open file for write: %s\n", in.GetFileName())
-		return &uploadPb.FileUploadResponse{Success: false, Message: "File failed to upload: could not open file for write"}, err
-	}
-	defer output.Close()
-
-	_, err = output.Write(in.GetFileData())
-	if err != nil {
-		log.Printf("Could not write file: %s\n", in.GetFileName())
-		return &uploadPb.FileUploadResponse{Success: false, Message: "Failed to write file."}, err
-	}
-	return &uploadPb.FileUploadResponse{Success: true, Message: "Successfully uploaded"}, nil
->>>>>>> c9bc42aeb0a21c9618460309c831caf7669ab5f3
 }
 
 func main() {

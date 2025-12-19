ec715c31cf8ff2e8b78a076992a9c1adca342529
jukebox <5196313+TheJukebox@users.noreply.github.com>
2025-12-20 00:13:19 +1100

    Seeking and volume controls

--- frontend/src/lib/components/Controls.svelte
+++ frontend/src/lib/components/Controls.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { Play, Pause, SkipForward, SkipBack, Volume, Volume1, Volume2 } from '@lucide/svelte';
+    import { Play, Pause, SkipForward, SkipBack, Volume, Volume1, Volume2, VolumeX } from '@lucide/svelte';
     import { trackList } from '$lib/tracklist.svelte';
-    import { togglePlayback, restartPlayback, playback } from '$lib/playback.svelte';
+    import { togglePlayback, restartPlayback, playback, setGain, startPlaybackAt } from '$lib/playback.svelte';
+    import { audioContext } from '$lib/stream';
+
+    let dragging: boolean = false;
+    let volumeSlider: HTMLDivElement;
+    let seekSlider: HTMLDivElement;
+    let offset: number = 0;
 
     function formatTime(seconds) {
         const h = Math.floor(seconds / 3600);
@@ -15,7 +21,70 @@
             String(s).padStart(2, '0'),
         ].join(':');
     };
+
+    function seek(e: MouseEvent) {
+        if (!seekSlider) return;  
+
+        const rect = seekSlider.getBoundingClientRect();
+        const x = Math.max(0, Math.min(e.clientX - rect.left, rect.width));
+        // we need to calculate the offset in seconds based on our progress across the bar
+        offset = (x / rect.width) * playback.duration;
+        if (playback.node) {
+            playback.node.stop();
+            playback.node = undefined;
+        }
+        if (playback.countInterval) {
+            clearInterval(playback.countInterval);
+        };
+        playback.timeElapsed = offset;
+        playback.startedAt = audioContext.currentTime - offset;
+    };
+
+    function setVolume(e: MouseEvent) {
+        if (!volumeSlider) return;
+
+        const rect = volumeSlider.getBoundingClientRect();
+        const x = Math.max(0, Math.min(e.clientX - rect.left, rect.width));
+        playback.volume = x / rect.width;
+        playback.gain?.gain.setValueAtTime(playback.volume, audioContext?.currentTime);
+        localStorage.setItem("volume", playback.volume.toString());
+    };
+
+    function mouseDown(e: MouseEvent) {
+        console.log(seekSlider.contains(e.target));
+        if (volumeSlider.contains(e.target)) {
+            dragging = true;
+            setVolume(e);
+        }
+        if (seekSlider.contains(e.target)) {
+            playback.seeking = true;
+            seek(e);
+        }
+    };
+
+    function mouseMove(e: MouseEvent) {
+        if (dragging) setVolume(e);
+        if (playback.seeking) seek(e);
+    };
+
+    function mouseUp(e: MouseEvent) {
+        dragging = false;
+        if (playback.seeking) {
+            playback.seeking = false;
+            startPlaybackAt(offset);
+        }
+    };
+
+    onMount(() => {
+        playback.volume = localStorage.getItem("volume") as number || 1;
+    });
+
 </script>
+
+<svelte:window
+    on:mousemove={mouseMove}
+    on:mouseup={mouseUp}
+/>
     
 <div
     class="grid grid-cols-[auto_1fr] gap-4 p-2 shadow bg-lime-50 rounded-xl w-full max-w-5xl"
@@ -25,8 +94,26 @@
     </div>
     <div class="flex flex-col gap-5 p-2 min-w-full shadow rounded-lg bg-lime-100 items-center">
         <div class="inset-shadow-sm/20 bg-lime-200 rounded-lg p-2 min-w-full min-h-30 transition">
-            <div class="h-2 rounded-full bg-slate-400 w-full shadow-sm/100">
-                <div class="h-2 rounded-full bg-lime-100" style={`width: ${(playback.timeElapsed / playback.duration) * 100}%`}></div>
+            <div bind:this={seekSlider} 
+                onmousedown={mouseDown}
+                role="slider"
+                aria-valuenow={playback.timeElapsed}
+                aria-valuemin={0}
+                aria-valuemax={1}
+                tabindex="0"
+                class="h-2 rounded-full bg-slate-400 w-full shadow-sm/100"
+            >
+                <div class="relative h-2 rounded-full bg-lime-100" style={`width: ${(playback.timeElapsed / playback.duration) * 100}%`}>
+                    <div 
+                        class="absolute -right-2 top-1/2 -translate-y-1/2 w-3.5 h-3.5 hover:scale-[1.25] bg-lime-400 rounded-full shadow-xs/40 hover:bg-lime-500"
+                        onmousedown={mouseDown}
+                        role="slider"
+                        aria-valuenow={playback.timeElapsed}
+                        aria-valuemin={0}
+                        aria-valuemax={1}
+                        tabindex="0"
+                    ></div>
+                </div>
             </div>
             <div class="px-4 py-1 float-right text-lime-800 text-sm">{formatTime(playback.timeElapsed)}/{formatTime(playback.duration) || "00:00:00"}</div>
             <div class="flex flex-col items-center justify-center text-center w-full transition">
@@ -39,15 +126,38 @@
         </div>
         <div class="relative flex items-center w-full">
             <div class="ml-15 flex items-center gap-2">
-                {#if playback.volume <= 50 }
-                    <Volume class="text-slate-500 fill-slate-500"/>
-                {:else if playback.volume <= 75 }
-                    <Volume1 class="text-slate-500 fill-slate-500"/>
-                {:else }
-                    <Volume2 class="text-slate-500 fill-slate-500"/>
-                {/if}
-                <div class="h-2 rounded-full bg-slate-400 w-30 shadow-sm/100">
-                    <div class="h-2 rounded-full bg-lime-100" style={`width: ${playback.volume}%`}></div>
+                <button onclick={() => setGain(0)}>
+                    {#if playback.volume <= 0 }
+                        <VolumeX class="text-slate-300 fill-slate-300"/>
+                    {:else if playback.volume <= 0.25 }
+                        <Volume class="text-slate-500 fill-slate-500"/>
+                    {:else if playback.volume <= 0.75 }
+                        <Volume1 class="text-slate-500 fill-slate-500"/>
+                    {:else }
+                        <Volume2 class="text-slate-500 fill-slate-500"/>
+                    {/if}
+                </button>
+                <div
+                    bind:this={volumeSlider}
+                    role="slider"
+                    aria-valuenow={playback.volume}
+                    aria-valuemin={0}
+                    aria-valuemax={1}
+                    tabindex="0"
+                    onmousedown={mouseDown}
+                    class="h-2 rounded-full bg-slate-400 w-30 shadow-sm/100"
+                >
+                    <div class={`h-2 rounded-full bg-lime-100 relative ${dragging ? "" : "transition-all"}`} style={`width: ${playback.volume * 100}%`}>
+                        <div 
+                            class="absolute -right-2 top-1/2 -translate-y-1/2 w-3.5 h-3.5 hover:scale-[1.25] bg-lime-400 rounded-full shadow-xs/40 hover:bg-lime-500"
+                            onmousedown={mouseDown}
+                            role="slider"
+                            aria-valuenow={playback.volume}
+                            aria-valuemin={0}
+                            aria-valuemax={1}
+                            tabindex="0"
+                        ></div>
+                    </div>
                 </div>
             </div>
             <div class="absolute left-1/2 transform -translate-x-1/2 flex items-center gap-4">
--- frontend/src/lib/playback.svelte.ts
+++ frontend/src/lib/playback.svelte.ts
@@ -3,8 +3,6 @@ import { StreamBuffer } from '$lib/stream';
 import type { StreamChunk } from '$lib/stream';
 import {audioContext, getAudioContext} from './stream';
 
-let counting: number = 0;
-
 export const playback = $state({ 
     playing: false,
     track: {},
@@ -15,27 +13,47 @@ export const playback = $state({
     gain: undefined,
     duration: 0,
     startedAt: 0,
+    offset: 0,
     timeElapsed: 0,
     volume: 1,
+    seeking: false,
+    countInterval: 0,
 });
 
 function countElapsed() {
-    playback.timeElapsed = (audioContext.currentTime - playback.startedAt);
+    playback.timeElapsed = (audioContext.currentTime - playback.startedAt + playback.offset);
     if (playback.timeElapsed == playback.duration) {
-        clearInterval(counting);
+        clearInterval(playback.countInterval);
+    }
+}
+
+export function setGain(value: number) {
+    if (playback.gain) {
+        playback.gain.gain.setValueAtTime(value, audioContext.currentTime);
+        playback.volume = value;
+        localStorage.setItem("volume", value.toString());
     }
 }
 
 export async function fadeOut(duration: number = 1) {
-    if (playback.node) {
-        playback.node.stop(duration);
+    if (playback.node && playback.gain) {
+        playback.gain.gain.linearRampToValueAtTime(0, audioContext.currentTime + duration);
+        playback.node.stop(audioContext.currentTime + duration);
     }
 }
 
-export async function restartPlayback() {
+export async function fadeIn(duration: number = 1) {
+    if (playback.node && playback.gain) {
+        playback.gain.gain.linearRampToValueAtTime(playback.volume, audioContext.currentTime + duration);
+    }
+}
+
+export async function startPlaybackAt(offset: number = 0) {
+    playback.timeElapsed = offset;
+    playback.offset = offset;
     playback.playing = false;
-    if (counting) {
-        clearInterval(counting);
+    if (playback.countInterval) {
+        clearInterval(playback.countInterval);
     }
     if (playback.node) { 
         playback.node.stop();
@@ -43,17 +61,64 @@ export async function restartPlayback() {
     };
     getAudioContext()
     if (playback.track && playback.audio && audioContext) {
-        playback.timeElapsed = 0;
-        playback.startedAt = Date.now();
+        // gain
+        playback.gain = audioContext.createGain();
+        playback.gain.gain.setValueAtTime(playback.volume, audioContext.currentTime);
+
+        // buffer
         playback.node = audioContext?.createBufferSource();
+        playback.node.connect(playback.gain);
         playback.node.buffer = playback.audio;
-        playback.node.connect(audioContext?.destination);
         playback.duration = playback.audio.duration; 
+
+        // playback
+        playback.gain.connect(audioContext.destination);
+        playback.node.start(audioContext.currentTime, offset);
+        audioContext.resume();
+        playback.playing = true;
+        playback.startedAt = audioContext.currentTime;
+        playback.countInterval = setInterval(countElapsed);
+
+        playback.node.onended = () => {
+            clearInterval(playback.countInterval);
+        };
+    }
+}
+
+export async function restartPlayback() {
+    playback.playing = false;
+    if (playback.countInterval) {
+        clearInterval(playback.countInterval);
+    }
+    if (playback.node) { 
+        playback.node.stop();
+        playback.node = undefined; 
+    };
+    getAudioContext()
+    if (playback.track && playback.audio && audioContext) {
+        // gain
+        playback.gain = audioContext.createGain();
+        playback.gain.gain.setValueAtTime(playback.volume, audioContext.currentTime);
+
+        // buffer
+        playback.node = audioContext?.createBufferSource();
+        playback.node.connect(playback.gain);
+        playback.node.buffer = playback.audio;
+        playback.duration = playback.audio.duration; 
+        playback.offset = 0;
+        playback.timeElapsed = 0;
+
+        // playback
+        playback.gain.connect(audioContext.destination);
         playback.node.start();
         audioContext.resume();
         playback.playing = true;
         playback.startedAt = audioContext.currentTime;
-        counting = setInterval(countElapsed);
+        playback.countInterval = setInterval(countElapsed);
+
+        playback.node.onended = () => {
+            clearInterval(playback.countInterval);
+        };
     }
 }
 
@@ -65,10 +130,10 @@ export function togglePlayback() {
     }
 }
 
-export async function startPlayback(track: Track) {
+export async function startPlayback() {
     playback.playing = false;
-    if (counting) {
-        clearInterval(counting);
+    if (playback.countInterval) {
+        clearInterval(playback.countInterval);
     }
     if (playback.node) { 
         playback.node.stop();
@@ -88,7 +153,6 @@ export async function startPlayback(track: Track) {
             temp.set(data, playable.byteLength);
             playable = temp;
         }
-        console.log(playable);
         playback.audio = await audioContext?.decodeAudioData(playable.buffer as ArrayBuffer);
 
         // gain
@@ -100,6 +164,8 @@ export async function startPlayback(track: Track) {
         playback.node.connect(playback.gain);
         playback.node.buffer = playback.audio;
         playback.duration = playback.audio.duration; 
+        playback.offset = 0;
+        playback.timeElapsed = 0;
 
         // playback
         playback.gain.connect(audioContext.destination);
@@ -107,7 +173,11 @@ export async function startPlayback(track: Track) {
         audioContext.resume();
         playback.playing = true;
         playback.startedAt = audioContext.currentTime;
-        counting = setInterval(countElapsed);
+        playback.countInterval = setInterval(countElapsed);
+
+        playback.node.onended = () => {
+            clearInterval(playback.countInterval);
+        };
     } else {
         console.error("Could not fetch audio context for playback!");
     }
